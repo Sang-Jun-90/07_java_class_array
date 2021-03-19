07b894a3c7e58e5c7d93a79855a0ca9b3b6dc620
19시_웹개발_LSJ <19__LSJ@DESKTOP-VD237BJ>
2021-03-19 21:42:05 +0900

    12

--- 07_java_class_array/src/step7_01/classArray/ClassArrayEx13_문제.java
+++ 07_java_class_array/src/step7_01/classArray/ClassArrayEx13_문제.java
@@ -1,5 +1,11 @@
 package step7_01.classArray;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -22,27 +28,48 @@ class StudentEx{
 
 class Manager{
 	
-	Student [] list = null;
+	StudentEx[] list = null;
 	int stdCnt = 0;
 	
 	void add_StudentEx(StudentEx st) {
 		
 		if (stdCnt == 0) {
-			list = new StudentEx[1];
+		list = new StudentEx[1];
+	}
+	else if (stdCnt > 0) {
+		StudentEx[] temp = list;
+		list = new StudentEx[stdCnt + 1];
+		for (int i = 0; i < stdCnt; i++) {
+			list[i] = temp[i];
 		}
-		else if (stdCnt > 0) {
-			StudentEx[] temp = list;
-			for (int i = 0; i < stdCnt; i++) {
-				list[i] = temp[i];
-			}
-			temp = null;
-		}
-		list[stdCnt] = st;
-		stdCnt++;
+		temp = null;
+	}
+	list[stdCnt] = st;
+	stdCnt++;
 		
 	}
 	
-	StudentEx remove_StudentEx (int index) {}
+	StudentEx remove_StudentEx (int index) {
+		
+		StudentEx temp = list[index];
+		
+		if (stdCnt == 1) list = null;
+		else if (stdCnt > 1) {
+			StudentEx[] tempList = list;
+			list = new StudentEx[stdCnt-1];
+			
+			for (int i = 0; i < index; i++) {
+				list[i] = tempList[i];
+			}
+			for (int i = index; i < stdCnt-1; i++) {
+				list[i] = tempList[i+1];
+			}
+			tempList = null;
+		}
+		stdCnt--;
+		return temp;
+		
+	}
 	
 	int check_id(StudentEx st) {
 		
@@ -57,13 +84,44 @@ class Manager{
 		
 	}
 	
-	void print_StudentEx() {}	
+	void print_StudentEx() {
+		
+		for (int i = 0; i < stdCnt; i++) {
+			list[i].print_data();
+		}
+		
+	}	
 	
-	String out_data() {}
+	String out_data() {
+		
+		String data = "";
+		if (stdCnt == 0) return data;
+		data+=stdCnt;
+		data+="\n";
+		
+		for (int i = 0; i < stdCnt; i++) {
+			data += list[i].id;
+			data += " , ";
+			data += list[i].pw;
+			if (stdCnt - 1 != i) {
+				data += "\n";
+			}
+			
+			
+		}
+		
+		return data;
+	}
 	
-	void sort_data() {}
+	void sort_data() {
+		
+		if (stdCnt < 2) System.out.println("정렬할게 없어요");
+		else {
+		}
+		
+	}
 	
-	void load_StudentEx (StudentEx [] temp , int count) {}
+//	void load_StudentEx (StudentEx [] temp , int count) {}
 	
 }
 
@@ -73,6 +131,7 @@ public class ClassArrayEx13_문제 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		Manager manager = new Manager();
 		
 		// Manager manager = new Manager();
 		
@@ -108,7 +167,7 @@ public class ClassArrayEx13_문제 {
 					System.out.print("비번은 : ");
 					temp.pw = scan.next();
 					
-					manager.add_studentEx(temp);
+					manager.add_StudentEx(temp);
 					System.out.println(temp.id + "님 환영해요.");
 					
 				}
@@ -118,11 +177,98 @@ public class ClassArrayEx13_문제 {
 				
 				
 			}
-			else if (sel == 2) {}
-			else if (sel == 3) {}
-			else if (sel == 4) {}
-			else if (sel == 5) {}
-			else if (sel == 6) {}
+			else if (sel == 2) {
+				manager.print_StudentEx();
+				StudentEx temp = new StudentEx();
+				System.out.println("아이디는?");
+				temp.id = scan.next();
+				int check = manager.check_id(temp);
+				
+				if (check == -1) {
+					System.out.println("없는 뎁쇼");
+				}
+				else {
+					StudentEx del_st = manager.remove_StudentEx(check);
+					System.out.println(del_st.id + "님 탈퇴되었어요.");
+				}
+				
+			}
+			else if (sel == 3) {
+				manager.sort_data();
+				manager.print_StudentEx();
+			}
+			else if (sel == 4) {
+				
+				manager.print_StudentEx();
+				
+			}
+			else if (sel == 5) {
+				
+				if (manager.stdCnt == 0) continue;
+				
+				try {
+					FileWriter fw = new FileWriter("StudentEx_manager.txt");
+					String data = manager.out_data();
+					if (!data.equals("")) {
+						fw.write(data);
+						System.out.println(data);
+					}
+					fw.close();
+					
+				} catch (IOException e) {e.printStackTrace();}
+				
+			}
+			else if (sel == 6) {
+				
+				File file = new File("StudentEx_manager.txt");
+				if (file.exists()) {
+					
+					try {
+						FileReader fr = new FileReader(file);
+						BufferedReader br = new BufferedReader(fr);
+						
+						String line = null;
+						try {
+							line = br.readLine();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						int count = Integer.parseInt(line);
+						StudentEx[] temp = new StudentEx[count];
+						
+						for (int i = 0; i < count; i++) {
+							temp[i] = new StudentEx();
+							try {
+								line = br.readLine();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							String value[] = line.split(",");
+							temp[i].id = value[0];
+							temp[i].pw = value[1];
+							
+						}
+						try {
+							br.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						try {
+							fr.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					} catch (FileNotFoundException e) {e.printStackTrace();}
+					
+				}
+				
+				
+			}
 			else if (sel == 7) { 
 				break; 
 			}
